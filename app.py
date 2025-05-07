67cef08fbee4f55a83d1248c5e9921cf0d41b183
Jiwon Choi <25876799+cjw531@users.noreply.github.com>
2025-05-06 20:19:14 -0700

    update youtube downloader logic

--- app.py
+++ app.py
@@ -51,6 +51,13 @@ def download_youtube_clip(url, start_time, end_time, output_filename="clip.mp4")
 
 st.title("YouTube Clip Downloader")
 
+# st.warning("경고: \
+#            다운로드 받은 동영상의 저작권 및 모든 관련 권리는 해당 크리에이터에게 귀속됩니다. \
+#            본 도구를 통해 획득한 영상을 무단 배포, 복제 또는 공개 공유하는 행위는 관련 저작권법을 위반할 수 있습니다. \
+#            관련법 위반에 따라 발생하는 모든 법적 책임은 전적으로 이용자에게 있습니다.")
+
+# st.markdown("YouTube 구간 다운로더에 오신 것을 환영합니다! YouTube 동영상 URL을 입력하고, 다운로드할 구간을 지정하세요.")
+
 st.header("1. Enter YouTube Video URL")
 url = st.text_input(label="Enter YouTube Video URL")
 
